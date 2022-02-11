3d12da58fbfec1ff40126893d4769a9a9f120db3
skela4 <42099128+skela4@users.noreply.github.com>
2022-02-11 13:58:26 +0100

    Add files via upload

--- whois.py
+++ whois.py
@@ -2,10 +2,33 @@
 import sys
 
 
-if __name__ == "__main__":
+def test_assertion(args):
     try:
-        args = sys.argv[1:]
+        num = args[0]
     except IndexError:
         raise SystemExit(f"Usage: {sys.argv[0]} INTEGER")
 
-    # if 
+    assert len(args) == 1, "more than one argument is provided"
+    assert (num.isdigit()), "argument is not integer"
+    return int(num, 10)
+
+
+def whois(num):
+    print(num)
+    if (num == 0):
+        print("I'm Zero")
+    elif (num % 2 == 0):
+        print("I'm Even")
+    else:
+        print("I'm Odd")
+
+
+if __name__ == "__main__":
+    try:
+        args = sys.argv[1:]
+        num = test_assertion(args)
+    except AssertionError as error:
+        assertion_type = type(error).__name__
+        print(f"{assertion_type}: {error}")
+    else:
+        whois(num)
