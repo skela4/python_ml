d64b6de4d7fca252acbbbb7ca8ca629b4b46f253
Aleksander AHIZI-ELLIAM <aahizi-e@e1r12p1.42.fr>
2022-03-14 16:14:05 +0100

    kata00

--- python-module-00/ex05/kata00.py
+++ python-module-00/ex05/kata00.py
@@ -1,23 +1,23 @@
-t = (19, 42, 21)
+t = (19,)
+
 
 def build_iterable(iterable):
     result = []
     try:
-        assert len(iterable) is not 0, "kata is empty"
         assert isinstance(iterable, tuple), "input is not of type tuple"
-        for integer in iterable:
+        assert len(iterable) is not 0, "kata is empty"
+        for elem in iterable:
             try:
-                assert isinstance(integer, int), "tuple can only be filled with integer"
+                assert isinstance(elem, int), "can only be filled with integer"
             except AssertionError as error:
                 raise SystemExit(f"{AssertionError.__name__}: {error}")
             else:
-                result.append(integer)
+                result.append(elem)
     except AssertionError as error:
         raise SystemExit(f"{AssertionError.__name__}: {error}")
-    if (len(result) is 1):
-        print(f"the {len(result)} number is: {', '.join(map(str, result))}")
-    else:
-        print(f"the {len(result)} numbers are: {', '.join(map(str, result))}")
+    typo = 'numbers are' if len(result) > 1 else 'number is'
+    print(f"the {len(result)} {typo}: {', '.join(map(str, result))}")
+
 
 if __name__ == "__main__":
     build_iterable(t)
