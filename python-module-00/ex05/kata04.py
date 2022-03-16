4803a5d7adae8842accaea45b3091c75da48fc4a
Aleksander AHIZI-ELLIAM <aahizi-e@e1r13p1.42.fr>
2022-03-16 17:14:06 +0100

    kata04

--- python-module-00/ex05/kata04.py
+++ python-module-00/ex05/kata04.py
@@ -3,21 +3,19 @@ kata = (0, 4, 132.4222, 10000, 12345.67)
 if __name__ == "__main__":
     try:
         assert isinstance(kata, tuple), "kata need to be a tuple"
-
         module, ex, decimal, integer, second_decimal = kata
-
-        assert module >= 0 and module < 100 and ex >= 0 and ex < 100, (
-            "first and third params need to be non-negative integer up to 2 digits"
+        assert all([isinstance(e, int) for e in [module, ex, integer]]), (
+            "first and second and fourth params "
+            "need to be integer"
         )
-        assert isinstance(ex, float) and isinstance(integer, float), (
-            "second and fourth params need to be non-negative"
+        assert all([e >= 0 and e < 100 for e in [module, ex]]), (
+            "first and second params "
+            "need to be a non-negative integer up to 2 digits"
         )
-        assert isinstance(decimal, int) and isinstance(second_decimal, int), (
-            "first and second params need to be non-negative"
+        assert all([isinstance(e, float) for e in [decimal, second_decimal]]), (
+            "third and fith param "
+            "need to be a decimal"
         )
-
-        print("module_{:02d}, ex_{:02d}, {:.2f}, {:.2e}, {:.2e}".format(*t))
+        print("module_{:02d}, ex_{:02d} : {:.2f}, {:.2e}, {:.2e}".format(*kata))
     except Exception as error:
         raise SystemExit(f"{type(error).__name__}: {error}")
-
-# titre ynov
