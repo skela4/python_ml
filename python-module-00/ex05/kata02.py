7065b659aee5c461945352be74d9ee68b710b66c
Aleksander AHIZI-ELLIAM <aahizi-e@e1r13p1.42.fr>
2022-03-16 15:07:57 +0100

    kata02

--- python-module-00/ex05/kata02.py
+++ python-module-00/ex05/kata02.py
@@ -1,14 +1,22 @@
 from datetime import datetime
 
+kata = (2019, 9, 25, 3, 30)
+
+
+def check_iterable(kata):
+    assert isinstance(kata, tuple), (
+        "input is not of type tuple"
+    )
+    assert all([isinstance(e, int) for e in kata]), (
+        "value in tuple can only be a integer"
+    )
+
 
 if __name__ == "__main__":
-    t = (3, 0, 19, 9, 25, 78, 789)
-    hour, minutes, year, month, day = t
     try:
-        print(
-            datetime(
-                year, month, day, hour, minutes).strftime("%m/%d/%Y %H:%M"))
-    except TypeError as msg:
-        print(msg)
-    except Exception as msg:
-        print(msg)
+        year, month, day, hour, minutes = kata
+        check_iterable(kata)
+        d = datetime(year, month, day, hour, minutes)
+        print(d.strftime('%m/%d/%Y %H/%M'))
+    except (AssertionError, ValueError) as error:
+        raise SystemExit(f"{type(error).__name__}: {error}")
