704d660964ecdcd677a29df590167d59c5d09b34
Aleksander AHIZI-ELLIAM <aahizi-e@e1r13p1.42.fr>
2022-03-18 03:59:35 +0100

    add pdf day00 && update recipe.py

--- python-module-00/ex06/recipe.py
+++ python-module-00/ex06/recipe.py
@@ -49,10 +49,12 @@ def print_recipe(recipe):
     elif recipe not in cookbook.keys():
         print("recipe not in cookbook")
     else:
-        print(f"Recipe for {recipe}:")
-        print(f"Ingredients list: {cookbook[recipe]['ingredients']}")
-        print(f"To be eaten for {cookbook[recipe]['meal']}")
-        print(f"Takes {cookbook[recipe]['prep_time']} minutes of cooking.")
+        print(
+            f"\nRecipe for {recipe}:\n"
+            f"  Ingredients list: {cookbook[recipe]['ingredients']}\n"
+            f"  To be eaten for {cookbook[recipe]['meal']}\n"
+            f"  Takes {cookbook[recipe]['prep_time']} minutes of cooking."
+        )
 
 
 def delete_recipe(recipe):
@@ -68,9 +70,9 @@ def add_recipe(recipe, ingredients, meal, prep_time):
     delimiters = "|".join(string.whitespace)
     recipe = recipe.strip(string.whitespace)
     meal = meal.strip(string.whitespace)
-    ingredients = re.sub(f'[{string.punctuation}]', '', ingredients)
-    ingredients = list(
-        filter(not_empty_string, re.split(delimiters, ingredients)))
+    # ingredients = re.sub(f'[{string.punctuation}]', '', ingredients)
+    # ingredients = list(
+    #     filter(not_empty_string, re.split(delimiters, ingredients)))
     prep_time = prep_time.strip(string.whitespace)
     if len(recipe) == 0:
         print("recipe name cannot be empty")
@@ -79,8 +81,10 @@ def add_recipe(recipe, ingredients, meal, prep_time):
     elif len(meal) == 0:
         print("meal name cannot be empty")
     elif not prep_time.isdigit() or len(prep_time) == 0:
-        print("all characters in the sting need to be a digit and need at \
-        least one character")
+        print(
+            "all characters in the sting need to be a digit and need at "
+            "least one character"
+            )
     elif recipe in cookbook.keys():
         print("recipe already in cookbook")
     else:
@@ -102,57 +106,13 @@ def print_all_recipes():
 
 
 def print_options(list_options):
-    print("Please select an option by typing the corresponding number:")
+    print("List of available option:")
     for option_number, option_text in list_options.items():
-        print(f"{option_number}: {option_text}")
+        print(f"  {option_number}: {option_text}")
+    print()
 
-
-def is_cookbook_valid():
-    default = ['ingredients', 'meal', 'prep_time', 'vfd']
-    try:
-        assert isinstance(cookbook, dict), "cookbook need to be dictionnary"
-        for recipe in cookbook:
-            assert isinstance(cookbook[recipe], dict), "recipe need to be dictionnary"
-            assert len(cookbook[recipe]) >= 3, "the recipe book need to store at least 3 couples key-value"
-            # for key in recipe.keys():
-            #     print(key)  
-            # result = [elem in default for elem in cookbook[recipe].keys()]
-            result = []
-            # for elem in cookbook[recipe].keys():
-            #     if elem in default:
-            #         result.append(True)
-            #     else:
-            #         result.append(False)
-            for elem in default:
-                if elem in cookbook[recipe].keys():
-                    result.append(True)
-                else:
-                    result.append(False)
-            
-                    # print(elem)
-            result = list(filter(lambda x: x == True, result))
-            assert len(result) >= 3, (
-                "need to have at least "
-                "'ingredients', 'meal', 'prep_time' as keys"
-            )
-            # result = all()
-            # print(result)
-
-            
-                # for ingredient in cookbook[recipe]:
-            # for ingredient in recipe:
-                # assert all([isinstance(e, str) for e in list_ingredient]), (
-                #     "each ingredients need to be a string"
-                # )
-
-
-    except AssertionError as error:
-        raise(SystemExit(f"{AssertionError.__name__}: {error}"))
 
 def program_cookbook():
-
-    is_cookbook_valid()
-    raise(SystemExit("STOP"))
     list_options = {
         '1': 'Add a recipe',
         '2': 'Delete a recipe',
@@ -161,21 +121,28 @@ def program_cookbook():
         '5': 'Quit'
         }
 
+    print("Welcome to the Python Cookbook !")
     print_options(list_options)
-    user_input = input()
+    user_input = input(
+        "Please select an option:\n"
+        ">> "
+        )
     while user_input != '5':
         while user_input is not None and user_input not in list_options.keys():
-            print("This option does not exist, \
-            please type the corresponding number.")
-            print("To exit, enter 5\n")
-            for option_number, option_text in list_options.items():
-                print(f"{option_number}: {option_text}")
-            user_input = input()
+            print("\nSorry this option does not exist.")
+            print_options(list_options)
+            user_input = '5'
         if (user_input == '1'):
-            recipe = input("\nRecipe name:\n")
-            ingredients = input("ingredients:\n")
-            meal_type = input("Type of meal:\n")
-            prep_time = input("Time of prep:\n")
+            recipe = input("\n>>> Enter a name:\n")
+            ingredients = []
+            ingredients.append(input("\n>>> Enter ingredients:\n"))
+            next_ingredient = input()
+            while (next_ingredient != ''):
+                ingredients.append(next_ingredient)
+                next_ingredient = input()
+
+            meal_type = input("\n>>> Enter a meal type:\n")
+            prep_time = input("\n>>> Enter a preparation time:\n")
             add_recipe(recipe, ingredients, meal_type, prep_time)
             print()
         elif (user_input == '2'):
@@ -183,16 +150,17 @@ def program_cookbook():
             delete_recipe(recipe)
             print()
         elif (user_input == '3'):
-            recipe = input('\nRecipe name:\n\n')
+            recipe = input("\nPlease enter a recipe name to get its details:\n>> ")
             print_recipe(recipe)
             print()
         elif (user_input == '4'):
             print("\n")
             print_all_recipes()
-            print()
-        print_options(list_options)
-        user_input = input()
-    print("\ncookbook closed")
+        user_input = input(
+            "\nPlease select an option:\n"
+            ">> "
+        )
+    print("\ncookbook closed. Goodbye !")
 
 
 if __name__ == "__main__":
