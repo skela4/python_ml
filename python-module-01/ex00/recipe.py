5a959cd0d780814ebde3e4b7bf883cde36cb1661
aahizi-e <aahizi-e@student.42.fr>
2022-03-26 03:34:31 +0100

    add assertion get recipe type

--- python-module-01/ex00/recipe.py
+++ python-module-01/ex00/recipe.py
@@ -1,11 +1,11 @@
 class Recipe:
-    def __init__(self, name, description = ''):
+    def __init__(self, name, recipe_type, description = ''):
         self.name = name
-        self.cooking_lvl = int
+        self.cooking_lvl = int(0)
         self.cooking_time = int
         self.description = description
-        self.ingredients = list
-        self.recipe_type = str
+        self.ingredients = list(['vdf','vfd','vfd', 'vfd'])
+        self.recipe_type = recipe_type
 
     def __str__(self):
         return self.description
@@ -17,8 +17,8 @@ class Recipe:
         return self.name
 
     def get_cooking_lvl(self):
-        assert isinstance(self.cooking_lvl, int), "(int) name need to be a integer"
-        assert self.cooking_lvl >= 0 and self.cooking_lvl <= 5 , "(int) range from 1 to 5"
+        assert isinstance(self.cooking_lvl, int), "cooking level need to be a integer"
+        assert self.cooking_lvl >= 0 and self.cooking_lvl <= 5 , "range from 1 to 5"
         return self.cooking_lvl
 
     def get_cooking_time(self):
@@ -27,22 +27,28 @@ class Recipe:
         return self.cooking_time
 
     def get_ingredients(self):
-        # assert isinstance(self.ingredients, list), "(list) list of all ingredients each represented by a string"
-        # [assert isinstance(ingredient, str) for ingredient in self.ingredients]
-
-        print([isinstance(ingredient, str) for ingredient in self.ingredients])
-        # assert all([isinstance(e, int) for e in kata]), (
-        #     "value in tuple can only be a integer"
+        to_assert = [isinstance(self.ingredients, list)]
+        [to_assert.append(isinstance(e, str)) for e in self.ingredients]
+        assert all(to_assert), "list of all ingredients each represented by a string"
+        return self.ingredients
+    
+    def get_recipe_type(self):
+        accept_recipe_type = ['starter', 'lunch', 'dessert']
+        to_assert = [self.recipe_type == type_recipe for type_recipe in accept_recipe_type]
+        assert any(to_assert), 'can be "starter", "lunch" or "dessert" '
+        # assert self.recipe_type == 'starter' or self.recipe_type == 'lunch'  or self.recipe_type == 'dessert' , (
+        #     "list of all ingredients each represented by a string"
         # )
-        return self.ingredient
+        return self.recipe_type
 
 
 if __name__ == '__main__':
-    tourte = Recipe('tourte', 'une simple tourte')
+    tourte = Recipe('tourte', 'starter', 'une simple tourte')
     try:
         name = tourte.get_name()
         print(name)
         tourte.get_cooking_lvl()
         tourte.get_ingredients()
+        tourte.get_recipe_type()
     except AssertionError as error:
         raise SystemExit(f"{AssertionError.__name__}: {error}")
